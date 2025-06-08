0b8afe4a1fbd307c6d1c153fcb6971bd3c92e509
Ryan Daigle <ryan.daigle@gmail.com>
2025-06-08 18:40:17 -0400

    Update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OpenAI.Agents.MixProject do
   def project do
     [
       app: :openai_agents,
-      version: "0.1.0",
+      version: "0.1.2",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
